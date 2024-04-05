ac6cc903419d09631465c7d9242a21d55cb72934
Elaidzha1940 <eli080293@gmail.com>
2024-04-05 23:05:07 +0300

    ChatBotMessenger

---
+++ ChatBotMessenger/ChatBotMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBotMessengerApp.swift
+//  ChatBotMessenger
+//
+//  Created by Elaidzha Shchukin on 05.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatBotMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatBotMessenger/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ChatBotMessenger
+//
+//  Created by Elaidzha Shchukin on 05.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
